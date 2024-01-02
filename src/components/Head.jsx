323fd3bc62825e831525c22ddec4e5b6b54bf2d4
Bhavesh Thakur <64855358+Bhaveshthakur-348@users.noreply.github.com>
2024-01-03 00:31:49 +0530

    Header created

---
+++ src/components/Head.jsx
@@ -0,0 +1,33 @@
+const Head = () => {
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          className="h-8 mx-2"
+          alt="youtube-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+        />
+      </div>
+      <div className="col-span-9 px-10">
+        <div className="flex justify-center">
+          <input
+            className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+            type="text"
+          />
+          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+            🔍
+          </button>
+        </div>
+      </div>
+      <div className="flex col-span-2 justify-end">
+        <img
+          className="h-8"
+          alt="user"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
