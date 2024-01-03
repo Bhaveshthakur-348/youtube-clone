21a47b954d7a52d4e0f459224abad71cbf1f502a
Bhavesh Thakur <64855358+Bhaveshthakur-348@users.noreply.github.com>
2024-01-03 22:20:06 +0530

    Routing added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,11 @@
-// import { Outlet } from "react-router-dom";
-import MainContainer from "./MainContainer";
+import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 
 const Body = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <MainContainer />
+      <Outlet />
     </div>
   );
 };
